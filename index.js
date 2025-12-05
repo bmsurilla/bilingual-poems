236ec33ad6ec4e329899025bd601e4b284efceaf
bmsurilla <bm.surilla@gmail.com>
2025-12-05 12:49:42 +0100

    added the AI api integration

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: `Alors avance, entre viento y suerte, <br/>
-cada paso te lleva, rompiendo la inerte. <br/>
-Hacia la liberté que en toi se cache— <br/>
-l’aventure empieza cuando tú te lâches.`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let languageInput1 = document.querySelector("#input-language-1");
+  let languageInput2 = document.querySelector("#input-language-2");
+
+  let apiKey = "bab44a6ef3at298bof0b63093865ccef";
+  let prompt = `generate a biligual poem with the two languages given by the user: ${languageInput1.value} and ${languageInput2.value}`;
+  let context =
+    "You are a learned linguist who loves poems. Create a limerick with the given languages by the user. Use HTML and add line breaks for added structure. Skip the backticks and html only display the poem. Please also sign it.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  alert("generating poem...");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#input-form");
